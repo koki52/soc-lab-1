4e85567014f048b6ef56b2058e674f714bd8018f
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-05 21:27:06 +0100

    Added security configuration

---
+++ src/main/java/hr/fer/zemris/rznu/lab1/config/SecurityConfiguration.java
@@ -0,0 +1,48 @@
+package hr.fer.zemris.rznu.lab1.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/h2-console/**").permitAll()
+                .antMatchers("/register").permitAll()
+                .antMatchers("/secure").authenticated()
+                .antMatchers("/logout").permitAll()
+                .antMatchers(HttpMethod.POST).authenticated()
+                .antMatchers(HttpMethod.PUT).authenticated()
+                .antMatchers(HttpMethod.DELETE).authenticated()
+                .anyRequest().permitAll()
+                .and().httpBasic();
+
+        http.csrf().disable();
+        http.logout().deleteCookies("JSESSIONID");
+        http.headers().frameOptions().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password,TRUE from user where username=?"
+                )
+                .authoritiesByUsernameQuery(
+                        "select username,'USER' from user where username=?"
+                );
+    }
+}
