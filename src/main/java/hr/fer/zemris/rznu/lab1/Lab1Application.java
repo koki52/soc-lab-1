b1cf7a5288efd68d9d34a60116ed035c2e2ccad9
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-05 21:25:14 +0100

    Enabled autoconfiguration

--- src/main/java/hr/fer/zemris/rznu/lab1/Lab1Application.java
+++ src/main/java/hr/fer/zemris/rznu/lab1/Lab1Application.java
@@ -1,9 +1,11 @@
 package hr.fer.zemris.rznu.lab1;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class Lab1Application {
 
 	public static void main(String[] args) {
