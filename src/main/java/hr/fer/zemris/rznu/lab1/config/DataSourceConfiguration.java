1ac1dd5244be41a8174685ba3bfaab62293391b4
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-05 21:26:48 +0100

    Added datasource configuration

---
+++ src/main/java/hr/fer/zemris/rznu/lab1/config/DataSourceConfiguration.java
@@ -0,0 +1,20 @@
+package hr.fer.zemris.rznu.lab1.config;
+
+import org.springframework.boot.autoconfigure.jdbc.DataSourceBuilder;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+
+import javax.sql.DataSource;
+
+@Configuration
+public class DataSourceConfiguration {
+
+    @Bean
+    @Primary
+    @ConfigurationProperties(prefix = "spring.datasource")
+    public DataSource dataSource() {
+        return DataSourceBuilder.create().build();
+    }
+}
