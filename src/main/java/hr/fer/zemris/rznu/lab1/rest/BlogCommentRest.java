f3352ec9b0ecf126cd546076be753c916d8f5fd7
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-06 01:21:33 +0100

    Added api prefix to routes

--- src/main/java/hr/fer/zemris/rznu/lab1/rest/BlogCommentRest.java
+++ src/main/java/hr/fer/zemris/rznu/lab1/rest/BlogCommentRest.java
@@ -14,7 +14,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping(path = "/comments")
+@RequestMapping(path = "/api/comments")
 public class BlogCommentRest {
 
     private BlogCommentDao blogCommentDao;
--- src/main/java/hr/fer/zemris/rznu/lab1/rest/UserRest.java
+++ src/main/java/hr/fer/zemris/rznu/lab1/rest/UserRest.java
@@ -7,14 +7,13 @@ import hr.fer.zemris.rznu.lab1.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
 
 @RestController
+@RequestMapping(path = "/api")
 public class UserRest {
 
     private UserUtil userUtil;
