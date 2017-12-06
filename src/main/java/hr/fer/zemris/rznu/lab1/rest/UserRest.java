2652ac23f10fbcefbdb2b92391f08e9ba801b556
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-06 01:49:43 +0100

    Removed debug method

--- src/main/java/hr/fer/zemris/rznu/lab1/rest/UserRest.java
+++ src/main/java/hr/fer/zemris/rznu/lab1/rest/UserRest.java
@@ -3,7 +3,6 @@ package hr.fer.zemris.rznu.lab1.rest;
 import hr.fer.zemris.rznu.lab1.dao.UserDao;
 import hr.fer.zemris.rznu.lab1.model.User;
 import hr.fer.zemris.rznu.lab1.util.Error;
-import hr.fer.zemris.rznu.lab1.util.UserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
