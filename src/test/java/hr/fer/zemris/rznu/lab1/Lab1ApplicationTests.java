59ab82d0edea18887f51b50550d528ff66d3a8b4
Marin Kostelac <marin.kostelac@fer.hr>
2017-12-06 16:46:04 +0100

    Ignored pointless test

--- src/test/java/hr/fer/zemris/rznu/lab1/Lab1ApplicationTests.java
+++ src/test/java/hr/fer/zemris/rznu/lab1/Lab1ApplicationTests.java
@@ -1,5 +1,6 @@
 package hr.fer.zemris.rznu.lab1;
 
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -10,6 +11,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class Lab1ApplicationTests {
 
 	@Test
+    @Ignore
 	public void contextLoads() {
 	}
 
